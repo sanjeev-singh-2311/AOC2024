7956d1a84520e07381b6a55057aacf98be7644d6
Sanjeev Singh <sanjeev.2311.singh@gmail.com>
2024-12-05 16:14:27 +0530

    Changed Day 5 P1 to use sorting instead of full comparision

--- Day5/P1.cpp
+++ Day5/P1.cpp
@@ -1,8 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+unordered_map<int, pair<unordered_set<int>, unordered_set<int>>> M;
+
+bool comparator(int a, int b) {
+    auto entry1 = M[a];
+    auto entry2 = M[b];
+    if (entry2.second.find(a) != entry2.second.end())
+        return false;
+    if (entry1.first.find(b) != entry1.first.end())
+        return false;
+    return true;
+}
+
+vector<int> makeValid(vector<int> V) {
+    sort(V.begin(), V.end(), comparator);
+    return V;
+}
+
 int main() {
-    unordered_map<int, pair<unordered_set<int>, unordered_set<int>>> M;
     string S;
     while (cin >> S) {
         if (S[2] != '|')
@@ -32,29 +48,10 @@ int main() {
             a.push_back(S[i]);
         }
         V.push_back(stoi(a));
-
-        bool isValid = true;
-        for (int i = 0; i < V.size(); i++) {
-            auto entry = M[V[i]];
-            for (int j = i - 1; j >= 0; j--) {
-                if (entry.second.find(V[j]) != entry.second.end()) {
-                    isValid = false;
-                    break;
-                }
-            }
-            if (!isValid)
-                break;
-            for (int j = i + 1; j < V.size(); j++) {
-                if (entry.first.find(V[j]) != entry.first.end()) {
-                    isValid = false;
-                    break;
-                }
-            }
-            if (!isValid)
-                break;
-        }
-        if (isValid)
+        vector<int> temp = makeValid(V);
+        if (temp == V)
             res += V[V.size() / 2];
+
     } while (cin >> S);
     cout << res << endl;
 }
